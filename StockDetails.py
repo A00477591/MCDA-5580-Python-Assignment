72d8b52bfa71b9a1449a5b214efc44a4509d3218
Royston Furtado <royston.furtado@smu.ca>
2024-04-15 02:02:37 -0300

    Update1

--- StockDetails.py
+++ StockDetails.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import requests
-import json
 from datetime import datetime
+import matplotlib.pyplot as plot
+from matplotlib.dates import DateFormatter
 
 def main():
     st.title("Stock Details App")
@@ -10,15 +11,32 @@ def main():
         coinsListResponse=requests.get("https://api.coingecko.com/api/v3/coins/list")
         if coinsListResponse.status_code==200:
             jsonResponse=coinsListResponse.json()
-            cryptocurrencies=[crypto['name'] for crypto in jsonResponse]
-            if any(userInput.lower()==cryptocurrency.lower() for cryptocurrency in cryptocurrencies):
+            cryptocurrencies=[(crypto['id'],crypto['name']) for crypto in jsonResponse]
+            crypto=next((cryptocurrency for cryptocurrency in cryptocurrencies if userInput.lower()==cryptocurrency[1].lower()),None)
+            if crypto:
+                print(crypto)
                 #Plot the coins price over the last year
-                historicalRatesResponse=requests.get("https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=CAD&days=365&precision=2")
+                historicalRatesResponse=requests.get(f"https://api.coingecko.com/api/v3/coins/{crypto[0]}/market_chart?vs_currency=CAD&days=365&precision=2")
                 if historicalRatesResponse.status_code==200:
-                    prices=historicalRatesResponse.json()['prices']
-                    print(datetime.fromtimestamp(float("1681430400000")).strftime('%Y-%m %H:%M:%S'))
+                    priceDetails=historicalRatesResponse.json()['prices']
+                    dates,prices=[],[]
+                    for priceDetail in priceDetails:
+                        dates.append(datetime.fromtimestamp(float(priceDetail[0])/1000).date())
+                        prices.append(priceDetail[1])
+                    fig,ax=plot.subplots()
+                    ax.plot(dates,prices)
+                    plot.gca().xaxis.set_major_formatter(DateFormatter('%b %Y'))
+                    plot.gca().yaxis.set_major_formatter(plot.FuncFormatter(lambda x,_:int(x)))
+                    ax.set_xlabel("Dates")
+                    ax.set_ylabel("Prices")
+                    ax.set_title(userInput+" prices over the last year")
+                    st.pyplot(fig)
+                    highestPrice=max(prices)
+                    lowestPrice=min(prices)
+                    st.write(f"<p style='font-size: 24px;'>{userInput} traded the highest on <b>{dates[prices.index(highestPrice)].strftime('%d %B %Y')}</b> at the price <b>{highestPrice}</b></p>",unsafe_allow_html=True)
+                    st.write(f"<p style='font-size: 24px;'>{userInput} traded the lowest on <b>{dates[prices.index(lowestPrice)].strftime('%d %B %Y')}</b> at the price <b>{lowestPrice}</b></p>",unsafe_allow_html=True)
                 else:
-                    st.error("Error while fetching the historical prices of the cryptocurrency. Please try again.")
+                    st.error("Error while fetching the historical prices of "+userInput+". Please try again.")
             else:
                 st.error("Invalid cryptocurrency!")
         else:
@@ -26,6 +44,3 @@ def main():
 
 if __name__=='__main__':
     main()
-
-# datetimeStr=datetime.fromtimestamp(float("1681516800000")/1000)
-# print(datetimeStr.strftime('%Y-%m'))
